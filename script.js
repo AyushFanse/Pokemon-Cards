8e0e421658fde12781dc8df85598e3ffda7c1636
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2021-10-01 14:05:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,70 @@
+//Defining the API.
 let url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=5`;
 let lastUrl = `https://pokeapi.co/api/v2/pokemon?offset=50&limit=5`;
-const po_container = document.querySelector('.po_container');//Selecting the class of po_container
-let pokeElement=document.createElement('div');// creating  div and adding needed values to it.
+
+//Selecting the class of po_container.
+const po_container = document.querySelector('.po_container');
+
+// creating  div and adding needed values to it.
+let pokeElement=document.createElement('div');
+
+//Defining the veriables
 let pokeInnerHTML =" ";
 let pokeGivenData =" ";
 
 
 
 const takePoke = async () => {
-        await getPoke();
+
+    //Using .catch() function to catch the errors.
+    await getPoke().catch(error => {alert(`Their is some problem in loading...!!! Please wait for a while.`)});;
 }
 
 
 
-
+//using async-await.
 const getPoke = async pokeGivenData => {
-    const res = await fetch(url);
-    const pokemonDetails = await res.json();//Using .json() to convert given text data into data objects as attribute-value pair and arrys.
+
+    //using catch to checking for errors.
+    const res = await fetch(url).catch(error => {alert(`Their is some problem while fatching the ${url}`)});
+
+    //Using .json() to convert given text data into data objects as attribute-value pair and arrys.
+    const pokemonDetails = await res.json();
     const pokeName =pokemonDetails.results;
     
-    // creating a loop to print all five pokemons at a time
- 
+    // creating a loop to print all five pokemons at a time.
     for (let i=0; i <5; i++) 
     {
         const pokeUrl = pokeName[i].url;
-        const Name =pokeName[i].name.toUpperCase(); // Getting name of the pokemon
-        const res1 = await fetch(pokeUrl);
-        const newUrl = await res1.json();//Using .json() to convert given text data into data objects as attribute-value pair and arrys.
+
+        // Getting name of the pokemon.
+        const Name =pokeName[i].name.toUpperCase();
+
+        //using catch to checking for errors.
+        const res1 = await fetch(pokeUrl).catch(error => {alert(`Their is some problem while fatching the ${url}`)}); 
+
+        //Using .json() to convert given text data into data objects as attribute-value pair and arrys.
+        const newUrl = await res1.json(); 
         const pokeData = newUrl.abilities;
         const moves = newUrl.moves;
         const index = newUrl.id;
         const weight = newUrl.weight;
-        let pokeMove = "";
+        let pokeMove = " ";
 
-        // using for loop to collect all Moves Data
-
+        // using for loop to collect all Moves Data.
         for (let j=0; j < moves.length; j++) 
             {
                 pokeMove +=moves[j].move.name[0].toUpperCase()+moves[j].move.name.slice(1)+", ";
             }
-        let temp ="";
+        let temp =" ";
 
-        // using for loop to collect all Ability Data
-
+        // using for loop to collect all Ability Data.
         for (let a=0; a < pokeData.length; a++)
             {
                 temp+=pokeData[a].ability.name[a].toUpperCase()+pokeData[a].ability.name.slice(1) + ", ";
             }
-                
         
-        
-        // Creating HTML for the card data
-
+        // Creating HTML for the card data.
         const dataPoke = 
         `<div class="img-container">
                 <table class="table">
@@ -84,24 +97,21 @@ const getPoke = async pokeGivenData => {
                 </table>
         </div>`;
 
-        // adding dollected data in the pokeInnerHTML element
+        // Collecting the data in the pokeInnerHTML veriable.
         pokeInnerHTML+=dataPoke;
+
+        // Using innerHTML to put all the data of the pokeInnerHTML inside the pokeElement
         pokeElement.innerHTML=pokeInnerHTML;
+
+        //Apending the values of pokeElement to the po_container.
         po_container.append(pokeElement);
     } 
 }
 
-
-
-
-// calling the takePuke func
+// calling the takePuke func.
 takePoke();
 
-
-
-
-
-// Creating func for previous onclick event
+// Creating func for previous onclick event.
 const prePoka = async pokeGivenData => 
     {   
         pokeInnerHTML=" ";
@@ -120,12 +130,7 @@ const prePoka = async pokeGivenData =>
     }
    
 
-
-
-
-
-
-// Creating func for next onclick event
+// Creating func for next onclick event.
 const nextPoka = async pokeGivenData => 
     {   
         pokeInnerHTML=" ";
